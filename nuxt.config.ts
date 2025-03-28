ef201f4f05630ab396880f20d82628bb9a3a3998
Eyumul <eyuelmulugeta60@gmail.com>
2025-03-28 21:34:00 +0300

    - integrate hasura to the project - add stock query

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/apollo'],
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: process.env.HASURA_GRAPHQL_ENDPOINT,
+        httpLinkOptions: {
+          headers: {
+            'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET,
+          },
+        }
+      },
+    },
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,25 @@
 <template>
     <div>
         <p class="text-5xl font-bold text-green-500">Hello world</p>
+        <div v-for="stock in stocks.stock" :key="stock.id">
+            <p>Name: <span class="font-black text-blue-500">{{ stock.name }}</span></p>
+            <p>Amount: <span class="font-black text-blue-500">{{ stock.amount }}</span></p>
+            <hr/>
+        </div>
     </div>
 </template>
 
 <script setup>
-
+const query = gql`
+    query MyQuery {
+        stock {
+            amount
+            id
+            name
+        }
+    }
+`
+const { data: stocks } = await useAsyncQuery(query)
 </script>
 
 <style scoped>
