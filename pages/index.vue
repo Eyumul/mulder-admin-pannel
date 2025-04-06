1ceba0fe6b3c48570985f88987d065661197810a
Eyumul <eyuelmulugeta60@gmail.com>
2025-04-06 14:42:05 +0300

    + create a login page + add style to the loagin page - remove uncessary queries

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,17 @@
 <template>
-    <div class="flex flex-col justify-between items-center px-4 py-8 bg-[rgba(0,0,0,0.65)] min-h-screen">
+    <div class="flex flex-col justify-between items-center px-4 py-8 bg-[rgba(0,0,0,0.65)] min-h-dvh">
         <p class="text-4xl text-white font-thin">Mulder</p>
         <form class="flex flex-col w-full gap-4 mb-12">
             <input type="text" placeholder="Email"/>
             <input type="password" placeholder="Password"/>
-            <button class="flex items-center justify-center mt-8 p-2 rounded-[5px] text-lg text-white bg-primary-500">Login</button>
+            <button type="button" @click="router.push('/stocks')" class="flex items-center active:scale-90 justify-center mt-16 p-2 rounded-[5px] text-lg text-white bg-primary-500">Login</button>
         </form>
         <p class="text-xs text-white">© Mulder leather 2025</p>
     </div>
 </template>
 
 <script setup>
-const query = gql`
-    query MyQuery {
-        stock {
-            amount
-            id
-            name
-        }
-    }
-`
-const { data: stocks } = await useAsyncQuery(query)
+const router = useRouter()
 </script>
 
 <style scoped>
