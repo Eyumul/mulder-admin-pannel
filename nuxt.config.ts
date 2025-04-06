ad7915eb0ba7bd16f4d76baed5e22b52a657358e
Eyumul <eyuelmulugeta60@gmail.com>
2025-04-06 17:38:18 +0300

    + add supabase nuxt module ~ modify nuxt module to include supabase url and api key

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/apollo', '@nuxt/icon'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/apollo',
+    '@nuxt/icon',
+    '@nuxtjs/supabase'
+  ],
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+  },
   apollo: {
     clients: {
       default: {
