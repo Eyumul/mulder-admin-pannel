edf00d7a0615d1ec8fd3dd084c14ca36690edf3b
Eyumul <eyuelmulugeta60@gmail.com>
2025-04-06 14:37:26 +0300

    + add nuct icon pakage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/apollo'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/apollo', '@nuxt/icon'],
   apollo: {
     clients: {
       default: {
